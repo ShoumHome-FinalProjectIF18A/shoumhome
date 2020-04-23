9fb7fb48c70c263f0f49bf2e28091b8fe513edbb
fendi-riawan <53111473+fendi-riawan@users.noreply.github.com>
2020-04-23 12:17:45 +0700

    tentang_FendiRiawan
    Menampilkan pada textView mengenai tentang yang terdiri dari Nama, NIM, Kelas, dan Alamat.

---
+++ shoumhome/app/src/main/java/com/example/shoumhome/tentang_FendiRiawan.java
@@ -0,0 +1,14 @@
+package com.example.shoumhome;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class tentang_FendiRiawan extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tentang__fendi_riawan);
+    }
+}
