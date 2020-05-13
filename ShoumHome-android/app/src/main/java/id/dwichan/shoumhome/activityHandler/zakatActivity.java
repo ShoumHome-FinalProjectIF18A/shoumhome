cb39d77a54317a7213bd63b479722ed043670a12
fendi-riawan <53111473+fendi-riawan@users.noreply.github.com>
2020-05-13 21:47:20 +0700

    Fendi Riawan 18.11.0268 : Kalkulator Zakat
    Halaman Kalkulator Zakat
    
    1. Membuat Layout untuk Halaman Zakat Fitrah dengan nama zakat_main.xml
    2. Menyusun Desain pada zakat_main.xml Menggunakan Button, TextView, Spinner, dan EditText
    3. Membuat Class zakatActivity.java pada activityHandler
    4. Mengisi Spinner dengan Membuat Array dengan nama ajeniszakat pada String.xml

---
+++ ShoumHome-android/app/src/main/java/id/dwichan/shoumhome/activityHandler/zakatActivity.java
@@ -0,0 +1,13 @@
+package id.dwichan.shoumhome.activityHandler;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class zakatActivity {
+    Button btzakatfitrah;
+    EditText editText,editText1,editText2;
+}
