23869f8d5962bcfbb1825d57a286649279b4720a
fendi-riawan <53111473+fendi-riawan@users.noreply.github.com>
2020-06-02 08:53:16 +0700

    Fendi Riawan 18.11.0268 : Halaman Ganti Kata Sandi Baru
    1. Membuat Halaman Layout Ganti Kata Sandi Baru dengan nama activity_gantisandi.xml
    2. Update Halaman login_main.xml (Mengubah ImageView dan Memperbarui LinerLayout)
    3. Membuat Class GantiSandiActivity.java

---
+++ ShoumHome-android/app/src/main/java/id/dwichan/shoumhome/activityHandler/GantiSandiActivity.java
@@ -0,0 +1,9 @@
+package id.dwichan.shoumhome.activityHandler;
+
+import android.widget.Button;
+import android.widget.EditText;
+
+public class GantiSandiActivity {
+    EditText etsandi, etulang_sandi;
+    Button btn_lanjut;
+}
