a07cb764085d061f6bd3cd61ec6b0eeb12fa87c2
maratun1725 <52594711+maratun1725@users.noreply.github.com>
2020-06-04 21:51:38 +0700

    Mar'Atun Solehah 18.11.0017 : layout utama, menu daftar 1 dan menu daftar 2
    1 Membuat tampilan layout layar utama yang berisi button masuk dan daftar dengan file yang bernama activity_layar_utama.xml
    2. Membuat tampilan layout menu daftar 1 yang berisi tentang pengisian data dari email dan password dengan file yang bernama activity_menu_daftar1.xml
    3. 2. Membuat tampilan layout menu daftar 1 yang berisi tentang pengisian data  diri seperti nama lengkap, nomor ponsel, dan tanggal lahir dengan file yang bernama activity_menu_daftar2.xml

---
+++ ShoumHome-android/app/src/main/java/id/dwichan/shoumhome/Menu_Daftar1.java
@@ -0,0 +1,14 @@
+package id.dwichan.shoumhome;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Menu_Daftar1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu__daftar1);
+    }
+}
