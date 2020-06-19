b54d5b77a99a4dc120d7d6905a2c3ce010c6372f
fendi-riawan <53111473+fendi-riawan@users.noreply.github.com>
2020-06-19 15:41:07 +0700

    Fendi Riawan 18.11.0268 : Upgrade Tampilan Kajian Online dan Update zakatActivity.java
    1. Upgrade Tampilan Kajian Online pada kajian_main.xml, list_data_kajian.xml, ListKajianAdapter.java, dan KajianActivity.java
    2. Update Aksi zakatActivity.java

--- ShoumHome-android/app/src/main/java/id/shoumhome/android/activityHandler/zakatActivity.java
+++ ShoumHome-android/app/src/main/java/id/shoumhome/android/activityHandler/zakatActivity.java
@@ -1,11 +1,44 @@
 package id.shoumhome.android.activityHandler;
 
+import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.TextView;
 
-public class zakatActivity {
-    Button btzakatfitrah;
-    EditText editText,editText1,editText2;
-    TextView textView3, textView4, textView5, textView6, textView7;
+import androidx.appcompat.app.AppCompatActivity;
+import id.shoumhome.android.R;
+
+public class zakatActivity extends AppCompatActivity {
+    EditText input_pertama, input_kedua;
+    TextView hasil;
+    Spinner spjeniszakat;
+    Double v1,v2,hasil1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.zakat_main);
+
+        input_pertama = (EditText) findViewById(R.id.editText);
+        input_kedua = (EditText) findViewById(R.id.editText1);
+        hasil = (TextView) findViewById(R.id.textView7);
+    }
+
+    public void konver(){
+        //konversi inputan ke double
+        v1 = Double.parseDouble(input_pertama.getText().toString());
+        v2 = Double.parseDouble(input_kedua.getText().toString());
+    }
+
+    public void proseskali(View view) {
+        konver();
+        hasil1 = v1*v2;  //perhitungan
+        hasil.setText(Double.toString(hasil1));  //output
+    }
+
+    //TODO
+    //Menambahkan Aksi Spinner Jenis Zakat
 }
