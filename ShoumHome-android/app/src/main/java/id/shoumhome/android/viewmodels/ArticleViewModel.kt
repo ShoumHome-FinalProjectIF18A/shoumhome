411ebe8e6b248c956b9949d501d1db651493db74
maratun1725 <52594711+maratun1725@users.noreply.github.com>
2020-08-16 13:34:08 +0700

    update viewmodel

---
+++ ShoumHome-android/app/src/main/java/id/shoumhome/android/viewmodels/ArticleViewModel.kt
@@ -0,0 +1,152 @@
+package id.shoumhome.android.viewmodels
+
+import android.content.Context
+import android.os.Looper
+import android.util.Log
+import android.widget.Toast
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.loopj.android.http.AsyncHttpClient
+import com.loopj.android.http.AsyncHttpResponseHandler
+import com.loopj.android.http.RequestParams
+import com.loopj.android.http.SyncHttpClient
+import cz.msebera.android.httpclient.Header
+import id.shoumhome.android.ustadz.R
+import id.shoumhome.android.ustadz.adapters.ArticleAdapter
+import id.shoumhome.android.ustadz.models.Article
+import id.shoumhome.android.ustadz.preferences.CredentialPreference
+import org.json.JSONObject
+import java.text.SimpleDateFormat
+import java.util.*
+import kotlin.collections.ArrayList
+
+class ArticleViewModel : ViewModel() {
+
+    private var listArticle = MutableLiveData<ArrayList<Article>>()
+
+    fun setArticle(context: Context, searchQuery: String = ""): String? {
+        try {
+            Looper.prepare()
+        } catch (e: Exception) {
+        }
+        val credential = CredentialPreference(context)
+        var ret: String? = null
+        val url = context.resources.getString(R.string.server) + "api/articles"
+        val listItems = ArrayList<Article>()
+        val client = SyncHttpClient()
+
+        // Request Parameters
+        val params = RequestParams()
+        params.put("read", "0")
+        params.put("ustadz_mode", "1")
+        params.put("ustadz_id", credential.getCredential().username) // replace admin with Ustadz ID
+        params.put("query", searchQuery)
+
+        client.get(url, params, object : AsyncHttpResponseHandler() {
+            override fun onSuccess(statusCode: Int, headers: Array<out Header>?, responseBody: ByteArray?) {
+                val result = responseBody?.let { String(it) }!!
+                val resultObject = JSONObject(result)
+                val list = resultObject.getJSONArray("data")
+
+                Log.d("Isi", result)
+
+                for (i in 0 until list.length()) {
+                    val articleJSONObject = list.getJSONObject(i)
+                    val article = Article()
+
+                    with(articleJSONObject) {
+                        article.id = this.getString("id")
+                        article.title = this.getString("title")
+                        article.content = this.getString("content")
+                        article.hasImg = this.getString("has_img")!!.toBoolean()
+
+                        if (article.hasImg) {
+                            val extension = this.getString("extension")
+                            article.imgUrl = context.resources.getString(R.string.server) + "assets/articles/${article.id}.$extension"
+                        }
+
+                        val date = this.getString("post_date")
+                        val dateDue = SimpleDateFormat("yyyy-MM-dd HH:mm:ss", Locale.getDefault()).parse(date)!!
+                        val dateDueFormatted = SimpleDateFormat("dd MMM yyyy HH:mm", Locale.getDefault()).format(dateDue)
+
+                        article.post_date = dateDueFormatted
+                    }
+                    listItems.add(article)
+                    //adapter.notifyDataSetChanged()
+                }
+                listArticle.postValue(listItems)
+                ret = null
+            }
+
+            override fun onFailure(statusCode: Int, headers: Array<out Header>?, responseBody: ByteArray?, error: Throwable?) {
+                error?.printStackTrace()
+                ret = "${responseBody?.let { String(it) }}"
+            }
+        })
+        return ret
+    }
+
+    fun setArticleAsync(context: Context, adapter: ArticleAdapter, searchQuery: String = ""): Boolean {
+        var ret = false
+        val url = context.resources.getString(R.string.server) + "api/articles"
+        val listItems = ArrayList<Article>()
+        val client = AsyncHttpClient()
+
+        // Request Parameters
+        val params = RequestParams()
+        params.put("read", "0")
+        params.put("ustadz_mode", "1")
+        params.put("ustadz_id", "admin") // replace admin with Ustadz ID
+        params.put("query", searchQuery)
+
+        client.get(url, params, object : AsyncHttpResponseHandler() {
+            override fun onSuccess(statusCode: Int, headers: Array<out Header>?, responseBody: ByteArray?) {
+                val result = responseBody?.let { String(it) }!!
+                val resultObject = JSONObject(result)
+                val list = resultObject.getJSONArray("data")
+
+                Log.d("Isi", result)
+
+                for (i in 0 until list.length()) {
+                    val articleJSONObject = list.getJSONObject(i)
+                    val article = Article()
+
+                    with(articleJSONObject) {
+                        article.id = this.getString("id")
+                        article.title = this.getString("title")
+                        article.content = this.getString("content")
+                        article.hasImg = this.getString("has_img")!!.toBoolean()
+
+                        if (article.hasImg) {
+                            val extension = this.getString("extension")
+                            article.imgUrl = context.resources.getString(R.string.server) + "assets/articles/${article.id}.$extension"
+                        }
+
+                        val date = this.getString("post_date")
+                        val dateDue = SimpleDateFormat("yyyy-MM-dd HH:mm:ss", Locale.getDefault()).parse(date)!!
+                        val dateDueFormatted = SimpleDateFormat("dd MMM yyyy HH:mm", Locale.getDefault()).format(dateDue)
+
+                        article.post_date = dateDueFormatted
+                    }
+                    listItems.add(article)
+                    adapter.notifyDataSetChanged()
+                }
+                listArticle.postValue(listItems)
+                ret = true
+            }
+
+            override fun onFailure(statusCode: Int, headers: Array<out Header>?, responseBody: ByteArray?, error: Throwable?) {
+                Toast.makeText(context, "Failure!\n${error?.message}\n${responseBody?.let { String(it) }}\n${url}", Toast.LENGTH_SHORT).show()
+                error?.printStackTrace()
+                ret = false
+            }
+        })
+        return ret
+    }
+
+    fun getArticle(): LiveData<ArrayList<Article>> {
+        return listArticle
+    }
+
+}
---
+++ ShoumHome-android/app/src/main/java/id/shoumhome/android/viewmodels/KajianViewModel.kt
@@ -0,0 +1,139 @@
+package id.shoumhome.android.viewmodels
+
+import android.content.Context
+import android.os.Looper
+import android.widget.Toast
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.loopj.android.http.AsyncHttpClient
+import com.loopj.android.http.AsyncHttpResponseHandler
+import com.loopj.android.http.RequestParams
+import com.loopj.android.http.SyncHttpClient
+import cz.msebera.android.httpclient.Header
+import id.shoumhome.android.ustadz.R
+import id.shoumhome.android.ustadz.adapters.KajianAdapter
+import id.shoumhome.android.ustadz.models.Kajian
+import org.json.JSONObject
+import java.text.SimpleDateFormat
+import java.util.*
+import kotlin.collections.ArrayList
+
+class KajianViewModel : ViewModel() {
+
+    private var listKajian = MutableLiveData<ArrayList<Kajian>>()
+
+    fun setKajian(context: Context, searchQuery: String = ""): String? {
+        try {
+            Looper.prepare()
+        } catch (e: Exception) {
+        }
+        var ret: String? = null
+        val url = context.resources.getString(R.string.server) + "api/kajian"
+        val listItems = ArrayList<Kajian>()
+        val client = SyncHttpClient()
+
+        // Request Parameters
+        val params = RequestParams()
+        params.put("read", "0")
+        params.put("ustadz_mode", "1")
+        params.put("ustadz_id", "admin") // replace admin with Ustadz ID
+        params.put("q", searchQuery)
+
+        client.get(url, params, object : AsyncHttpResponseHandler() {
+            override fun onSuccess(statusCode: Int, headers: Array<out Header>?, responseBody: ByteArray?) {
+                val result = responseBody?.let { String(it) }!!
+                val resultObject = JSONObject(result)
+                val list = resultObject.getJSONArray("data")
+
+                for (i in 0 until list.length()) {
+                    val kajianJSONObject = list.getJSONObject(i)
+                    val kajian = Kajian()
+
+                    with(kajianJSONObject) {
+                        kajian.id = this.getString("id")
+                        kajian.title = this.getString("kajian_title")
+                        kajian.place = this.getString("place")
+                        kajian.address = this.getString("address")
+                        kajian.mosque = this.getString("mosque_name")
+
+                        val date = this.getString("date_due")
+                        val dateDue = SimpleDateFormat("yyyy-MM-dd HH:mm:ss", Locale.getDefault()).parse(date)!!
+                        val dateDueFormatted = SimpleDateFormat("dd MMM yyyy HH:mm", Locale.getDefault()).format(dateDue)
+
+                        kajian.date = dateDueFormatted
+                        kajian.imgResource = this.getString("img_resource")
+                    }
+                    listItems.add(kajian)
+                    //adapter.notifyDataSetChanged()
+                }
+                listKajian.postValue(listItems)
+                ret = null
+            }
+
+            override fun onFailure(statusCode: Int, headers: Array<out Header>?, responseBody: ByteArray?, error: Throwable?) {
+                error?.printStackTrace()
+                ret = "${responseBody?.let { String(it) }}"
+            }
+        })
+        return ret
+    }
+
+    fun setKajianAsync(context: Context, adapter: KajianAdapter, searchQuery: String = ""): Boolean {
+        var ret = false
+        val url = context.resources.getString(R.string.server) + "api/kajian"
+        val listItems = ArrayList<Kajian>()
+        val client = AsyncHttpClient()
+
+        // Request Parameters
+        val params = RequestParams()
+        params.put("read", "0")
+        params.put("ustadz_mode", "1")
+        params.put("ustadz_id", "admin") // replace admin with Ustadz ID
+        params.put("q", searchQuery)
+
+        client.get(url, params, object : AsyncHttpResponseHandler() {
+            override fun onSuccess(statusCode: Int, headers: Array<out Header>?, responseBody: ByteArray?) {
+                val result = responseBody?.let { String(it) }!!
+                val resultObject = JSONObject(result)
+                val list = resultObject.getJSONArray("data")
+
+                for (i in 0 until list.length()) {
+                    val kajianJSONObject = list.getJSONObject(i)
+                    val kajian = Kajian()
+
+                    with(kajianJSONObject) {
+                        kajian.id = this.getString("id")
+                        kajian.title = this.getString("kajian_title")
+                        kajian.place = this.getString("place")
+                        kajian.address = this.getString("address")
+                        kajian.mosque = this.getString("mosque_name")
+
+                        val date = this.getString("date_due")
+                        val dateDue = SimpleDateFormat("yyyy-MM-dd HH:mm:ss", Locale.getDefault()).parse(date)!!
+                        val dateDueFormatted = SimpleDateFormat("dd MMM yyyy HH:mm", Locale.getDefault()).format(dateDue)
+
+                        kajian.date = dateDueFormatted
+                        kajian.imgResource = this.getString("img_resource")
+                    }
+                    listItems.add(kajian)
+                    adapter.notifyDataSetChanged()
+                }
+                listKajian.postValue(listItems)
+                ret = true
+            }
+
+            override fun onFailure(statusCode: Int, headers: Array<out Header>?, responseBody: ByteArray?, error: Throwable?) {
+                Toast.makeText(context, "Failure!\n${error?.message}\n${responseBody?.let { String(it) }}\n${url}", Toast.LENGTH_SHORT).show()
+                error?.printStackTrace()
+                ret = false
+            }
+        })
+        return ret
+    }
+
+    fun getKajian(): LiveData<ArrayList<Kajian>> {
+        return listKajian
+    }
+
+}
